94e0d256cb3c3139b8093bf146e11dfeef878b69
Kaan Özsavran <kaanozsavran@hotmail.com>
2025-08-09 21:47:46 +0300

    added data extraction and loading process

--- course-project/src/App.jsx
+++ course-project/src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Courses from "./Courses";
+import Loading from "./Loading";
 
 function App() {
-  return <div>asddsaads</div>;
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchCourses = async () => {
+    setLoading(false);
+    try {
+      const response = await axios.get("http://localhost:3000/courses");
+      setCourses(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? <Loading /> : <Courses courses={courses} />}
+    </div>
+  );
 }
 
 export default App;
--- course-project/src/Courses.jsx
+++ course-project/src/Courses.jsx
@@ -1,4 +1,4 @@
-function Courses() {
+function Courses({ courses }) {
   return <div></div>;
 }
 
