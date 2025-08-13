53f5a5106f9430e42d5fe5ea372e6ff1b89a14de
Kaan Özsavran <kaanozsavran@hotmail.com>
2025-08-13 15:35:35 +0300

    added single card structure and placement of icons

--- course-project/src/Courses.jsx
+++ course-project/src/Courses.jsx
@@ -1,13 +1,31 @@
 import Course from "./Course";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 function Courses({ courses, removeCourse }) {
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
   return (
     <div className="courseMainDiv">
       <div>
         <h2>Kurslarım</h2>
       </div>
       <div className="cardDiv">
-        {courses.map((course) => {
+        <button className="prevNextBtn">
+          <FaChevronLeft />
+        </button>
+        <div className="card">
+          <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <h4 className="cardPrice">{price}TL</h4>
+          </div>
+          <p>{content}</p>
+        </div>
+        <button className="prevNextBtn">
+          <FaChevronRight />
+        </button>
+
+        {/* {courses.map((course) => {
           return (
             <Course
               key={course.id}
@@ -16,7 +34,7 @@ function Courses({ courses, removeCourse }) {
             />
           );
           // course={course}
-        })}
+        })} */}
       </div>
     </div>
   );
